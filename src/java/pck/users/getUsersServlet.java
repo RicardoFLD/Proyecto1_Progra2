e7c17ed388dbfdd01aa62973d9091382f6640706
angiebermudezm <101956966+angiebermum@users.noreply.github.com>
2022-11-21 10:43:32 -0600

    Actualización GetUsers

--- src/java/pck/users/getUsersServlet.java
+++ src/java/pck/users/getUsersServlet.java
@@ -75,29 +75,6 @@ public class getUsersServlet extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
 
-            /*out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProductsServlet</title>");
-            out.println("<script src=\"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js\" integrity=\"sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3\" crossorigin=\"anonymous\"></script>");
-            out.println("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js\" integrity=\"sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz\" crossorigin=\"anonymous\"></script>");
-            out.println("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT\" crossorigin=\"anonymous\">");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<div class=\"container\" style=\"margin-top:5%\">");
-            out.println("   <h2>User list</h2>");
-            out.println("   <div class=\"list-group\">");
-
-            while(resultSet.next()){
-                out.println("<a href=\"#\" class=\"list-group-item list-group-item-action\">" + resultSet.getString("Name") + " " + resultSet.getString("LastName") + "</a>");
-            }
-            
-            out.println("   <div class=\"list-group\">");
-            out.println("   <hr/>");
-            out.println("   <a href=\"index.html\">Go back</a>");
-            out.println("</div>");
-            out.println("</body>");
-            out.println("</html>");*/
         } catch (NumberFormatException | ClassNotFoundException | SQLException e) {
             out.println(e.getMessage());
         }
