d38c9d5e668a70ed7fa9e432c3121152059ca9a3
angiebermudezm <101956966+angiebermum@users.noreply.github.com>
2022-11-21 10:26:19 -0600

    Agregación de servlet

--- src/java/pck/users/insertUserServlet.java
+++ src/java/pck/users/insertUserServlet.java
@@ -2,20 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package Users;
+package pck.users;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author PRINCIPAL
- */
-public class Users extends HttpServlet {
+
+public class insertUserServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -26,20 +29,44 @@ public class Users extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Users</title>");            
-            out.println("</head>"); 
-            out.println("<body>");
-            out.println("<h1>Servlet Users at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            
+            String txtName = request.getParameter("txtName");
+            String txtEmail = request.getParameter("txtEmail");
+            int txtPhone = Integer.parseInt(request.getParameter("txtPhone"));
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/WebUsers", "root", "Admin$1234");
+            Statement statement = connection.createStatement();
+            Statement statement2 = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from users where Email = '" + txtEmail + "'");
+
+            if (resultSet.next()) {
+                out.println("<script type='text/javascript'>alert('Email already used');</script>");
+                RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+                rd.include(request, response);
+            } else {
+                String sql = "insert into users (Name, Email, Phone) "
+                        + "values (" + txtName + "', '" + txtEmail + "', '" + txtPhone + "')";
+
+                statement2.executeUpdate(sql);
+                statement2.close();
+
+                out.println("<script type='text/javascript'>alert('User created');</script>");
+                RequestDispatcher rd = request.getRequestDispatcher("/getUsersServlet");
+                rd.include(request, response);
+            }
+
+            statement.close();
+        } catch (NumberFormatException | ClassNotFoundException | SQLException e) {
+            out.println(e.getMessage());
         }
     }
 
