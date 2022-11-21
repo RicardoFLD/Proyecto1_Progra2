a10fb89691ab6d2eb1b9806b2bed3322a2ce5fe9
RicardoFLD <63625002+RicardoFLD@users.noreply.github.com>
2022-11-21 14:29:33 -0600

    cambios en servlets

--- src/java/pck/users/insertUserServlet.java
+++ src/java/pck/users/insertUserServlet.java
@@ -17,7 +17,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+/**
+ *
+ * @author PRINCIPAL
+ */
 public class insertUserServlet extends HttpServlet {
 
     /**
